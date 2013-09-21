5dcbcc4cb9d7d14f13872354da86e351e09b327f
Mayank Jain <mjmayank@gmail.com>
2013-09-21 17:12:48 -0400

    location

--- moment/ImageViewController.h
+++ moment/ImageViewController.h
@@ -7,11 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ImageViewController : UIViewController
+@interface ImageViewController : UIViewController<CLLocationManagerDelegate>
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView2;
 @property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 - (void) setCameraPic:(UIImage *)image;
 
--- moment/ImageViewController.m
+++ moment/ImageViewController.m
@@ -34,6 +34,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     //self.imageView.image = self.image;
+    self.locationManager=[[CLLocationManager alloc] init];
+    self.locationManager.desiredAccuracy=kCLLocationAccuracyBest;
+    self.locationManager.delegate=self;
+    [self.locationManager startUpdatingLocation];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -49,6 +53,29 @@
 
 - (void) setCameraPic:(UIImage *)image{
     self.imageView2.image = image;
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation
+{
+//    Foursquare *foursquare = [[Foursquare alloc] initWithClientID:@"I2J4JN5GIOMDNSFP02H3CWYTQ0L0CXMRTVRXSWVR3XRZCCJ2"
+//                                                     clientSecret:@"H5MDABPQUUGLG25LEDYXH3JKBYIVGQMHALQ1VUAA2HKHSCLC"];
+    
+    NSLog([NSString stringWithFormat:@"%@, %@", newLocation, oldLocation]);
+    
+    const CLLocationDegrees latitude = newLocation.coordinate.latitude;
+    const CLLocationDegrees longitude = newLocation.coordinate.longitude;
+    
+    NSString * query = @"";
+    
+//    [foursquare searchVenuesWithLatitude:latitude longitude:longitude query:query completionHandler:^(NSDictionary *results, NSError *error) {
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            
+//            NSDictionary * searchResults = [[results objectForKey:@"response"] objectForKey:@"venues"];
+            //            [self.view.tableView reloadData];
+//        });
+//    } ];
 }
 
 @end
