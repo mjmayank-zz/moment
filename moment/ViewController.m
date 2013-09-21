f0e739954d369d66935b76616276944de30add85
Mayank Jain <mjmayank@gmail.com>
2013-09-21 18:22:09 -0400

    not really sure whats in this one

--- moment/ViewController.m
+++ moment/ViewController.m
@@ -34,6 +34,22 @@
 
 - (void)viewDidAppear:(BOOL)animated
 {
+    if (![PFUser currentUser]) { // No user logged in
+        // Create the log in view controller
+        PFLogInViewController *logInViewController = [[PFLogInViewController alloc] init];
+        [logInViewController setDelegate:self]; // Set ourselves as the delegate
+        
+        // Create the sign up view controller
+        PFSignUpViewController *signUpViewController = [[PFSignUpViewController alloc] init];
+        [signUpViewController setDelegate:self]; // Set ourselves as the delegate
+        
+        // Assign our sign up controller to be displayed from the login controller
+        [logInViewController setSignUpController:signUpViewController];
+        
+        // Present the log in view controller
+        [self presentViewController:logInViewController animated:YES completion:NULL];
+    }
+    
     [self.navigationController setNavigationBarHidden:YES animated:NO];
 }
 
